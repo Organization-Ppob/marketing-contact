149aceeb73cf5ce75eeef9e08c75e5aa288b69df
Muhammad Alfiandi Rizki <alfian.jw@gmail.com>
2026-02-11 11:51:58 +0700

    truested host

--- src/auth.ts
+++ src/auth.ts
@@ -13,6 +13,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     process.env.NEXTAUTH_SECRET ||
     "default-secret-change-in-production",
 
+  trustHost: true,
   providers: [
     Credentials({
       name: "Credentials",
